cb2f28eb8223dad52cb491107d1081c3f58a81c1
Christina Koch <christinakconnect@gmail.com>
2026-04-02 11:32:34 -0500

    notebook and script updates

--- generate_table.sh
+++ generate_table.sh
@@ -2,14 +2,14 @@
 
 TABLE=jobtable.csv
 
-# check if table exists, otherwise ask if we want it to be removed or add a -f flag
+# eventually check if table exists, otherwise ask if we want it to be removed or add a -f flag
 
-HEADER="executable,arguments,infile,output_directory,request_cpus,request_memory,request_disk"
+HEADER="executable,arguments,infile,outfile,request_cpus,request_memory,request_disk"
 echo $HEADER > $TABLE
 
 for f in inputs/*
 do
- echo ${f}
+ #echo ${f}
  ID=`echo ${f} | cut -d / -f 2 | cut -d . -f 1`
- echo "/opt/conda/bin/python3,example.py $ID,$ID.csv,results,1,2GB,2GB" >> $TABLE
+ echo "/opt/conda/bin/python3,example.py $ID,$ID.csv,$ID.png,1,2GB,2GB" >> $TABLE
 done
